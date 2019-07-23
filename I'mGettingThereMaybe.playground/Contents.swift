7bb4e6d4aed95bee3b3d0df4599f53e89ac8fdf2
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-23 10:59:14 -0400

    New Dawn New Day

---
+++ I'mGettingThereMaybe.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var miFamilia = ["Stephanie": "Mother", "John": "Father", "Patricia": "Paternal Grandmother", "Jacklynn": "Materal Grandmother", "Tom Sr.": "Maternal Grandfather", "Mary": "Paternal Aunt(younger)", "Deb": "Paternal Aunt(older)", "Tom Jr": "Maternal Uncle", "Michele": "Maternal Aunt(in-law)", "Andrew": "Paternal Uncle(in-law)", "Poppy Pi": "Doggo"]
+print(miFamilia)
+print(Array(miFamilia.keys)[3])
+print(Array(miFamilia.keys)[4])
+print(Array(miFamilia.keys)[8])
+
+
