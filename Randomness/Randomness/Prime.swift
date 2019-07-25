b3eb0f3061537c0361876ec02dd427b4a03f3f4f
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-25 09:40:10 -0400

    I failed

---
+++ Randomness/Randomness/Prime.swift
@@ -0,0 +1,18 @@
+//
+//  Prime.swift
+//  Randomness
+//
+//  Created by Apple on 7/25/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+func prime() {
+    var number = 0
+    
+    if (number/2..<Int.any) = (Int) {
+        print(false)
+    }
+    
+}
