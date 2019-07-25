0848255878f4203bf7ca1bba7f239212d58141e1
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-25 13:31:05 -0400

    Boom Shia

--- StoryboardYikes/StoryboardYikes/ViewController.swift
+++ StoryboardYikes/StoryboardYikes/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pageTitle: UILabel!
+    
+    @IBOutlet weak var changeTitle: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        if let newTitle = changeTitle.text {
+            pageTitle.text = newTitle
+        }
     }
 
 
